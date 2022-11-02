af5516ea7605f2b6573b8b1a8d2594a181c7968e
jlnorma4 <jlnorma4@asu.edu>
2022-11-01 20:41:34 -0500

    WIP-troubleshooting, application works

--- fielddayproto/src/App.js
+++ fielddayproto/src/App.js
@@ -1,9 +1,299 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+var apiKey = "apikey=a2abf1ee&";
+let mainId = '';
+let respSpace = document.getElementById("search-by-id-response");
+let reqSpace = document.getElementById("search-by-id-request");
+let recomSpace = document.getElementById("recommendations");
+let breadCrumbs = [];
+let termHist = [];
+
+
+function getRequestObject() {
+  if (window.XMLHttpRequest) {
+    return(new XMLHttpRequest());
+  } else {
+    return(null);
+  }
+}
+function crumbHist(terms){
+  var start = terms.indexOf('&');
+  if (start > 0){
+    terms = terms.substring(start,terms.length);
+  }
+  termHist.push(terms);
+  var html = '';
+  var entries = sessionStorage.getItem("entries");
+  entries = JSON.parse(entries);
+
+  for (var key in entries){
+    html += "<a href =# onclick= breadQuery('"+ encodeURI(entries[key])+
+        "')>"+ termHist[key] +"> </a>";
+  }
+
+  document.getElementById("breadCrumbs").innerHTML=html;
+
+}
+
+function breadQuery(address){
+  sessionStorage.clear();
+  breadCrumbs = [];
+  termHist = [];
+  ajaxResult(decodeURI(address));
+}
+
+function ajaxResult(address) {
+  var request = getRequestObject();
+  // var encAdd = encodeURI(address);
+  breadCrumbs.push(address);
+  sessionStorage.setItem("entries", JSON.stringify(breadCrumbs));
+  let termURL = new URL(address);
+  let params = termURL.searchParams.toString();
+
+  crumbHist(params);
+  request.onreadystatechange =
+      function() { showResponseText(request); };
+  request.open("GET", address, true);
+  request.send(null);
+
+  ajaxRecs(address);
+}
+
+function ajaxRecs(address, mainId){
+
+  var request = getRequestObject();
+
+  request.onreadystatechange =
+      function(){showRecs(request, mainId);
+      };
+  request.open("GET",address,true);
+  request.send(null);
+}
+
+function showRecs(request, mainId){
+  if ((request.readyState == 4) &&
+      (request.status == 200)) {
+    var json = JSON.parse(request.response);
+    var html = '';
+    let recs = [];
+    let i = 0;
+    let count =0;
+
+    for (i ; count< 5; i++) {
+      // if(rating !== null){ html += '<li><b>Star Rating: '+ rating+ '</b></li>'}
+      if(mainId !== json.Search[i].imdbID) {
+
+        html = '<ul style = "list-style-type: none">';
+        if(localStorage.getItem(json.Search[i].imdbID) !== null){
+          html += '<li><b>Star Rating: '+localStorage.getItem(json.Search[i].imdbID)+'</b></li>'
+        }
+        html+=
+            '<li><b> Title: ' + json.Search[i].Title + '</b></li>' +
+            '<li><b>Year: ' + json.Search[i].Year + '</b></li>' +
+            '<li><b>Type: ' + json.Search[i].Type + '</b></li>' +
+            '<li><b> imdbID: ' + json.Search[i].imdbID + ' </b></li>' +
+            '<li><img src=' + json.Search[i].Poster + '></li></ul>';
+        recs.push(html);
+        count++;
+      }
+    }
+    var movieRec1 = document.getElementById("movie-rec-1");
+    var movieRec2 = document.getElementById("movie-rec-2");
+    var movieRec3 = document.getElementById("movie-rec-3");
+    var movieRec4 = document.getElementById("movie-rec-4");
+    var movieRec5 = document.getElementById("movie-rec-5");
+
+    movieRec1.innerHTML = recs[0];
+    movieRec2.innerHTML = recs[1];
+    movieRec3.innerHTML = recs[2];
+    movieRec4.innerHTML = recs[3];
+    movieRec5.innerHTML = recs[4];
+    recomSpace.setAttribute('style', "display: table");
+  }
+}
+
+function showResponseText(request) {
+  if ((request.readyState == 4) &&
+      (request.status == 200)) {
+
+    let keyURL = new URL(request.responseURL);
+    let params = keyURL.searchParams;
+    let title = params.get('t');
+    let year = params.get('y');
+    let idParam = params.get('i');
+    let plot = params.get('plot');
+    let resp = params.get('r');
+    let rating = '';
+
+
+
+    let searchTerm = "";
+
+    let safeURL = "http://www.omdbapi.com/?";
+    let recURL = "http://www.omdbapi.com/?";
+
+    if(title !== null){
+      safeURL = safeURL + "t=" + title;
+    }
+    if(year !== null){
+      safeURL = safeURL + "&y=" + year;
+    }
+    if(idParam !== null){
+      safeURL = safeURL + "i=" + idParam;
+    }
+    if(plot !== null){
+      safeURL = safeURL + "&plot=" + plot;
+    }
+    if(resp !== null){
+      safeURL = safeURL + "&r=" + resp;
+    }
+
+    if(request.responseXML !== null){
+      var movie = request.responseXML.getElementsByTagName("movie")[0];
+
+      mainId = movie.attributes[17].textContent;
+      rating = localStorage.getItem(mainId);
+      if(title !== null) {
+        searchTerm = 's=' + title;
+      }else{
+        const queryArr = movie.attributes[0].textContent.split(" ");
+        if(queryArr.length > 1) {
+          searchTerm = 's=' + queryArr[0] + " " + queryArr[1];
+        }else{
+          searchTerm = 's=' + queryArr[0]
+        }
+      }
+
+      var html = '<ul style = "list-style-type: none">';
+      html += "<img src = " + movie.attributes[13].textContent + "></a>";
+      if(rating !== null){
+        html += '<li><b>Star Rating: '+rating+'</b></li>';
+      }
+      for (var i = 0; i < movie.attributes.length; i++){
+
+        if(movie.attributes[i].name === "poster"){
+          html += '<li><b>' + movie.attributes[i].name + '</b>' + ': ' + "<a href=" + movie.attributes[i].textContent +">Poster</a></li>";
+        }else {
+          html += '<li><b>' + movie.attributes[i].name + '</b>' + ': ' + movie.attributes[i].textContent + '</li>'
+        }
+      }
+      html += '</ul>';
+      respSpace.querySelector('pre').innerHTML = html;
+    }else{
+      var json = JSON.parse(request.response);
+
+
+      if(title !== null) {
+        searchTerm = 's=' + title;
+      }else{
+        const queryArr = json["Title"].split(" ");
+        if(queryArr.length > 1) {
+          searchTerm = 's=' + queryArr[0] + " " + queryArr[1];
+        }else{
+          searchTerm = 's=' + queryArr[0]
+        }
+      }
+
+      mainId = json["imdbID"];
+      rating = localStorage.getItem(mainId);
+      var html = '<ul style = "list-style-type: none">';
+      html += "<img src = " + json["Poster"] + ">";
+      if(rating !== null){
+        html += "<li><b>Star Rating: "+ rating+ "</b></li>";
+      }
+      for(var key in json){
+
+        if(key === "Poster"){
+          html += '<li><b>' + key + '</b>' + ': ' + "<a href=" + JSON.stringify(json[key]) +">Poster</a></li>";
+        }else {
+          html += '<li><b>' + key + '</b>' + ': ' + JSON.stringify(json[key]) + '</li>'
+        }
+      }
+      html += '</ul>';
+      respSpace.querySelector('pre').innerHTML = html;
+    }
+
+    reqSpace.querySelector('a').setAttribute('href',request.responseURL);
+    reqSpace.querySelector('a').innerHTML = safeURL;
+    reqSpace.setAttribute('style',"display: inherit");
+    respSpace.setAttribute('style', "display: table");
+
+    recURL += apiKey + searchTerm;
+
+    ajaxRecs(recURL, mainId);
+
+  }
+}
+
+function titleReset(){
+  reqSpace.setAttribute('style', "display: none");
+
+  respSpace.setAttribute('style', 'display: none');
+
+  recomSpace.setAttribute('style', 'display: none');
+}
+
+function idReset(){
+  reqSpace.setAttribute('style', "display: none");
+
+  respSpace.setAttribute('style', 'display: none');
+
+  recomSpace.setAttribute('style', 'display: none');
+
+}
+
+function searchTitle(){
+
+  var apiAddy = "http://www.omdbapi.com/?";
+  apiAddy = apiAddy + apiKey;
+
+  let title = document.getElementById("t").value;
+  let year  = document.getElementById('y').value;
+  let plot = document.getElementById('p').value;
+  let respType = document.getElementById('r').value;
+
+  apiAddy = apiAddy + "t=" + title;
+
+  if (year !== ""){
+    apiAddy = apiAddy +"&y=" + year;
+  }
+  if (plot !== ""){
+    apiAddy = apiAddy + "&plot=" + plot;
+  }
+  if (respType !== ""){
+    apiAddy = apiAddy + "&r=" + respType;
+  }
+  ajaxResult(apiAddy)
+
+}
+
+function searchID(){
+  var apiAddy = "http://www.omdbapi.com/?";
+  apiAddy = apiAddy + apiKey;
+
+  let id = document.getElementById("i").value;
+  let plot = document.getElementById('p1').value;
+  let respType = document.getElementById('r1').value;
+
+  apiAddy = apiAddy + "i=" + id;
+
+  if (plot !== ""){
+    apiAddy = apiAddy + "&plot=" + plot;
+  }
+  if (respType !== ""){
+    apiAddy = apiAddy + "&r=" + respType;
+  }
+
+  ajaxResult(apiAddy)
+}
+
+function setRating(){
+  let rating = document.getElementById("rate").value;
+  // localStorage.setItem(mainId, rating);
+  localStorage.setItem(mainId, rating);
+}
 
 function App() {
-  render()
-  {
     return (
         // <div className="App">
         //   <header className="App-header">
@@ -21,7 +311,7 @@ function App() {
         //     </a>
         //   </header>
         // </div>
-        <div className="container">
+        <div className="App">
           <div className="bs-docs-section" id="examples">
             <div className="row">
               <div className="col-lg-12">
@@ -42,35 +332,37 @@ function App() {
                       <input type="text" id="t" name="t" className="input-small"/>
                       &nbsp;&nbsp;
                       <label className="control-label" htmlFor="y">Year:</label>
-                      <input type="text" id="y" name="y" className="input-small" style="width: 100px;"/>
+                      <input type="text" id="y" name="y" className="input-small" style=
+                          {{width: "100px"}}/>
                       &nbsp;&nbsp;
                       <label className="control-label">Plot:</label>
-                      <select id="p" name="plot" style="width: 100px;">
+                      <select id="p" name="plot" style=
+                          {{width: "100px"}}>
                         <option value="" selected="">Short</option>
                         <option value="full">Full</option>
                       </select>
                       &nbsp;&nbsp;
                       <label className="control-label">Response:</label>
-                      <select id="r" name="r" style="width: 100px;">
+                      <select id="r" name="r" style={{width: "100px"}}>
                         <option value="">JSON</option>
                         <option value="xml">XML</option>
                       </select>
                       &nbsp;&nbsp;
                       <button id="search-by-title-button" type="button" className="btn-sm btn-primary"
-                              onClick="searchTitle()">Search
+                              onClick={searchTitle}>Search
                       </button>
                       <button id="search-by-title-reset" type="reset" className="btn-sm"
-                              onClick="titleReset()">Reset
+                              onClick={titleReset}>Reset
                       </button>
                     </div>
-                    <div className="hide" id="search-by-title-request" style="display: none;">
+                    <div className="hide" id="search-by-title-request" style={{display: "none"}}>
                       <br/>
                       <p>Request:</p>
                       <pre className="alert alert-box"><a href="javascript:;" target="_blank"></a></pre>
                     </div>
-                    <div className="hide" id="search-by-title-response" style="display: none;">
+                    <div className="hide" id="search-by-title-response" style={{display: "none"}}>
                       <p>Response:</p>
-                      <pre className="alert alert-success" style="margin-bottom: 0px; white-space: normal;"></pre>
+                      <pre className="alert alert-success" style={{marginBottom: '0px',whiteSpace: "normal"}}></pre>
                     </div>
                   </form>
                 </div>
@@ -86,32 +378,32 @@ function App() {
                       <input type="text" id="i" name="i" className="input-small" placeholder="IMDb ID"/>
                       &nbsp;&nbsp;
                       <label className="control-label">Plot:</label>
-                      <select id="p1" name="plot" style="width: 100px;">
+                      <select id="p1" name="plot" style={{width: "100px"}}>
                         <option value="" selected="">Short</option>
                         <option value="full">Full</option>
                       </select>
                       &nbsp;&nbsp;
                       <label className="control-label">Response:</label>
-                      <select id="r1" name="r" style="width: 100px;">
+                      <select id="r1" name="r" style={{width: "100px"}}>
                         <option value="">JSON</option>
                         <option value="xml">XML</option>
                       </select>
                       &nbsp;&nbsp;
                       <button id="search-by-id-button" type="button" className="btn-sm btn-primary"
-                              onClick="searchID()">Search
+                              onClick={searchID}>Search
                       </button>
-                      <button id="search-by-id-reset" type="reset" className="btn-sm" onClick="idReset()">Reset</button>
+                      <button id="search-by-id-reset" type="reset" className="btn-sm" onClick={idReset}>Reset</button>
                     </div>
-                    <div className="hide" id="search-by-id-request" style="display: none;">
+                    <div className="hide" id="search-by-id-request" style={{display: "none"}}>
                       <br/>
                       <h2>Request:</h2>
                       <pre><a href="javascript:;" target="_blank"></a></pre>
                     </div>
-                    <div className="hide" id="search-by-id-response" style="display: none;">
+                    <div className="hide" id="search-by-id-response" style={{display: "none"}}>
                       <h2>Response:</h2>
-                      <pre style="margin-bottom: 12px; white-space: normal;"></pre>
+                      <pre style={{marginBottom: "12px", whiteSpace: "normal"}}></pre>
                       <label className="control-label">Rating:</label>
-                      <select id="rate" name="ratings" style="width: 100px;">
+                      <select id="rate" name="ratings" style={{width: "100px"}}>
                         <option value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -120,13 +412,13 @@ function App() {
                         <option value="5">5</option>
                       </select>
                       <button id="set-rating-button" type="button" className="btn-sm btn-primary"
-                              onClick="setRating()">Submit Rating
+                              onClick={setRating}>Submit Rating
                       </button>
                     </div>
                   </form>
-                  <div id="recommendations" style="display: none">
+                  <div id="recommendations" style={{display: "none"}}>
                     <h2>Others you may enjoy:</h2>
-                    <table style="width:100%">
+                    <table style={{width:"100%"}}>
                       <tr>
                         <td id="movie-rec-1">
                           damn movie goes here
@@ -157,7 +449,6 @@ function App() {
 
         </div>
     );
-  }
 }
 
 export default App;
